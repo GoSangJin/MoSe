0deecbe34b984e46f0dbea9d8ae4796d76f11a1d
권찬호 <cksghxkd@naver.com>
2024-09-23 12:48:27 +0900

    - 지역 분류

---
+++ src/main/java/com/woori/studylogin/Constant/RegionType.java
@@ -0,0 +1,22 @@
+package com.woori.studylogin.Constant;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum RegionType {
+    서울특별시,
+    인천광역시,
+    부산광역시,
+    경기도,
+    강원도,
+    충청북도,
+    충청남도,
+    전라북도,
+    전라남도,
+    경상북도,
+    경상남도,
+    제주특별자치도;
+}
+
