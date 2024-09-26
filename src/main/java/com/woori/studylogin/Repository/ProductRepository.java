b79419bef13aaa29209c52f2aba1f2932287e607
GoSangJin <rhtkdwls21@naver.com>
2024-09-26 17:11:51 +0900

    체험 0926 찐찐마지막

--- src/main/java/com/woori/studylogin/Repository/ProductRepository.java
+++ src/main/java/com/woori/studylogin/Repository/ProductRepository.java
@@ -1,5 +1,6 @@
 package com.woori.studylogin.Repository;
 
+import com.woori.studylogin.Constant.RegionType;
 import com.woori.studylogin.Constant.StatusType;
 import com.woori.studylogin.Entity.ProductEntity;
 import org.springframework.data.domain.Page;
@@ -10,6 +11,8 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface ProductRepository extends JpaRepository<ProductEntity, Integer> {
     Page<ProductEntity> findByStatus(StatusType status, Pageable pageable);
+    Page<ProductEntity> findByRegionType(RegionType regionType, Pageable pageable);
+
 }
 
 
