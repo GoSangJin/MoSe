4a6baff266844e087ef58498dcc3b1ddc7c4b1b1
권찬호 <cksghxkd@naver.com>
2024-10-16 16:18:34 +0900

    - 1016

--- src/main/java/com/woori/studylogin/Entity/UserEntity.java
+++ src/main/java/com/woori/studylogin/Entity/UserEntity.java
@@ -5,6 +5,7 @@ import jakarta.persistence.*;
 import lombok.*;
 
 import java.time.LocalDate;
+import java.util.List;
 
 @Getter
 @Setter
@@ -29,9 +30,20 @@ public class UserEntity extends BaseEntity {
     private String address; //주소
     private String detailAddress;//상세주소
     private String extraAddress; //참고주소
-    @Enumerated(EnumType.STRING) //열거형 적용을 문자형으로 
+    @Enumerated(EnumType.STRING) //열거형 적용을 문자형으로
     private RoleType roleType;
 
     private boolean isSuspended;
     private LocalDate suspensionEndDate;
+
+    // 유저가 삭제되면 유저의 게시글이 삭제
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<BoardEntity> boards;
+
+    // 유저가 삭제되면 유저의 댓글이 삭제
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<CommentEntity> comments;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<LikeEntity> likes;
 }
